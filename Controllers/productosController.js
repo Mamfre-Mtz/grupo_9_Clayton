950e5cdc902858af4fa7378968f722bc472281aa
Mamfre-Mtz <mamfre32614@gmail.com>
2021-07-31 21:36:23 -0700

    retrospectiva and sass compilator

--- Controllers/productosController.js
+++ Controllers/productosController.js
@@ -1,12 +1,18 @@
-const path = require('path');
-const controlador={
-    productos:(req,res)=>{
-        res.sendFile(path.join(__dirname, "../public/views/productos.html"));
-    },
+const path = require("path");
+const controlador = {
+  productos: (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/views/productos.html"));
+  },
 
-    carrito:(req,res)=>{
-        res.sendFile(path.join(__dirname,"../public/views/carrito.html"));
-    }
-}
+  carrito: (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/views/carrito.html"));
+  },
+
+  add: (req, res) => {},
+  store: (req, res) => {},
+  destroy: (req, res) => {},
+
+  single: (req, res) => {},
+};
 
 module.exports = controlador;
--- Routes/productosRoutes.js
+++ Routes/productosRoutes.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const router =  express.Router();
-const productosController = require('../Controllers/productosController');
+const express = require("express");
+const router = express.Router();
+const productosController = require("../Controllers/productosController");
 
-router.get('/productos',productosController.productos);
+router.get("/productos", productosController.productos);
 
-router.get('/carrito', productosController.carrito);
+router.get("/carrito", productosController.carrito);
+
+// Add One Product
+router.get("/crear", productosController.add);
+router.post("/", productosController.store);
+
+// Get One Product
+router.get("/:id", productosController.single);
+
+// Delete One Product
+router.get("/:id/borrar", productosController.destroy);
 
 module.exports = router;
